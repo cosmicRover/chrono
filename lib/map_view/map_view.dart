ee5762905af84f1497b8fc0019e16718306fccaa
joypaul <jpaul3250@gmail.com>
2020-06-27 08:11:02 -0400

    frame implemented

--- lib/map_view/map_view.dart
+++ lib/map_view/map_view.dart
@@ -1,6 +1,8 @@
+import 'package:chrono/resuable_widgets/frame1.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MapSample extends StatefulWidget {
   @override
@@ -24,18 +26,16 @@ class MapSampleState extends State<MapSample> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: GoogleMap(
+      body: Stack(children: [
+        GoogleMap(
         mapType: MapType.hybrid,
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
-      ),
+        Frame1(),
+      ],)
     );
   }
 
